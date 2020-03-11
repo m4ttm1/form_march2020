38a43c2def892d93f210c984f2d8d7b501badddd
Matthieu Pinto <matthieu.pinto@gmail.com>
2020-03-11 15:47:25 +0100

    chore:webpack config test mode build

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,71 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { BannerPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BannerPlugin } = require('webpack');
 
-const METADATA = 'Copyright Orsys 2020 new generation modern sites';
-
-/** @type {import ('webpack').Configuration} */
-const config = {
-  devtool: 'false',
-  entry: './src/js/main.js',
-  plugins: [
-    new HtmlWebPackPlugin({ template: './src/index.html' }),
-    new BannerPlugin({
-      banner: METADATA,
+function configFactory(_, { mode }) {
+  const plugins = [
+    new BannerPlugin(`Copyright ${new Date().getFullYear()} Orsys`),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
     }),
-    new MiniCssExtractPlugin({
-      filename: 'app.[hash].css',
-    }),
-  ],
+  ];
 
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        // make all files ending in .json5 use the `json5-loader`
-        test: /\.json5$/,
-        use: 'json5-loader',
-        type: 'javascript/auto',
-      },
-    ],
-  },
-};
+  if (mode === 'production') {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'app.[hash].min.css',
+      }),
+    );
+  }
 
-module.exports = config;
+  /** @type {import('webpack').Configuration} */
+  const config = {
+    devtool: mode !== 'production' ? 'source-map' : false,
+    entry: './src/js/main.js',
+    output: {
+      filename: mode !== 'production' ? 'app.js' : 'app.[hash].min.js',
+    },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.json5$/,
+          use: 'json5-loader',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+  };
+
+  return config;
+}
+
+module.exports = configFactory;
+
+// Exercice 1
+// Utiliser json5-loader pour permettre de charger des fichiers
+// au format .json5
+// https://webpack.js.org/loaders/json5-loader/
+
+// Exercice 2
+// Utiliser banner-plugin pour afficher dans une "bannière" un
+// copyright "Copyright 2020 Nous"
+// en haut de chaque fichier buildé
+// https://webpack.js.org/plugins/banner-plugin/$
+// tester avec : npm run build:dev
+
+// Exercice 3
+// Utiliser mini-css-extract-plugin pour extraire les fichiers css
+// dans un fichier externe et les charger via une balise link
+// https://webpack.js.org/plugins/mini-css-extract-plugin/
+// tester avec : npm run build:dev
